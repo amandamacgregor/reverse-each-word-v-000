e2cc715fea92febdb0a950e6ab91247c8e4295fd
Amanda M <amandaseagull3@gmail.com>
2018-03-30 16:03:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
 def reverse_each_word(sentence)
   sentence.split.map{ |each_word|
 end
+
+
+# def first_method_reverse_each_word(array)
+#   nuarray = []
+#   array.map! do |element|
+#     element.reverse
+#   end 
+# array.join(" ")
+# end 
